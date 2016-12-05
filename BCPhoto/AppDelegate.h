ca7a6a174c8aa4f29ef596b7341df80996b5efa6
BearJunior <admin@admindeimac-9.local>
2016-12-05 11:44:13 +0800

    Initial Commit

---
+++ BCPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BCPhoto
+//
+//  Created by admin on 16/12/5.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
